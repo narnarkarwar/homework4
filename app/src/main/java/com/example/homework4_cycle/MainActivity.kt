690759793106fc648f14423263a228445f7a2dfd
narnarkarwar <yyan3370@gmail.com>
2020-03-04 13:10:41 +0630

    First Commit

---
+++ app/src/main/java/com/example/homework4_cycle/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.homework4_cycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
